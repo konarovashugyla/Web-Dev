8cf485861965a9fef14ee1374f1f9b45b2bf879b
konarovashugyla <99074570+konarovashugyla@users.noreply.github.com>
2023-02-15 16:04:05 +0600

    Update todolist.js

--- lab3/todolist.js
+++ lab3/todolist.js
@@ -1,15 +1,28 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Simple To Do List</title>
+    <!--Google Font-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+    <!--Font Awesome CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="list.css">
+</head>
+<body>
+    <nav class="nav">
+        <h2>ToDo List</h2>
+    </nav>
+    <i class="uil uil-trash-alt"></i>
+    <div class="container">
+        <div id="newtask">
+            <h1>ToDo List</h1>
+            <input type="text" placeholder="New Task">
+            <button id="push">Add</button>
+        </div>
+        <div id="tasks"></div>
+    </div>
+    <!--Script-->
+    <script src="list.js"></script>
+</body>
+</html>
