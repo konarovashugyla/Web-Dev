c80a2b0e83ebf8d29fd230bb13cdcf01f75ce000
konarovashugyla <99074570+konarovashugyla@users.noreply.github.com>
2023-02-15 13:31:58 +0600

    Create variables.js

---
+++ lab3/variables.js
@@ -0,0 +1,16 @@
+//working with variables
+let admin, name;
+
+name="John";
+admin=name;
+alert(admin);
+
+//giving the right name
+let myplanet;
+myplanet="ZHZH";
+let currentvisitor="Zhanaru";
+
+//uppercase const
+const birthday = '18.04.1982';//we can use uppercase for BIRTHDAY (for constants)
+
+const age = someCode(birthday);
